13d7196325e7e1d004c34c03d83ed543986233f9
deepredk <deepred.kim@gmail.com>
2023-09-30 18:23:56 +0900

    Initial commit

---
+++ src/main/java/it/numble/hittracker/HitTrackerApplication.java
@@ -0,0 +1,13 @@
+package it.numble.hittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/numble/hittracker/HitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.numble.hittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
