311fc9123577dae9b4d55793e1445e42ec7440b6
Jinhong Kim <deepred.kim@gmail.com>
2023-10-17 18:47:31 +0900

    fix: 테스트용 내장 Redis 포트 오타 수정

--- src/test/java/it/numble/hittracker/service/HitTrackerServiceTests.java
+++ src/test/java/it/numble/hittracker/service/HitTrackerServiceTests.java
@@ -127,7 +127,7 @@ class TestRedisConfiguration {
     private final RedisServer redisServer;
 
     public TestRedisConfiguration() throws IOException {
-        this.redisServer = new RedisServer(6370);
+        this.redisServer = new RedisServer(6379);
     }
 
     @PostConstruct
