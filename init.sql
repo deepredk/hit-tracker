8dd42007736c1e5d7c462a39122f5e25e203aa13
Jinhong Kim <deepred.kim@gmail.com>
2023-10-17 19:56:05 +0900

    feat: redis & mysql docker-compose.yml 작성

---
+++ init.sql
@@ -0,0 +1,18 @@
+-- hittracker 데이터베이스 생성
+CREATE DATABASE IF NOT EXISTS hittracker;
+USE hittracker;
+
+-- Url 테이블 생성
+CREATE TABLE IF NOT EXISTS Url (
+    url_id INT AUTO_INCREMENT PRIMARY KEY,
+    url VARCHAR(255) NOT NULL UNIQUE
+);
+
+-- DailyHitLog 테이블 생성
+CREATE TABLE IF NOT EXISTS DailyHitLog (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    dailyHit INT NOT NULL,
+    url_id INT NOT NULL,
+    FOREIGN KEY (url_id) REFERENCES Url(url_id) ON DELETE CASCADE
+);
